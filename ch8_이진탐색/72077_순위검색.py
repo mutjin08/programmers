2d800f9a518c23621368e5d6e4ee983c8b7c5b8a
mutjin08 <mutjin08@gmail.com>
2024-01-17 17:36:18 +0900

    24-01-17

--- ch8_이진탐색/72077_순위검색.py
+++ ch8_이진탐색/72077_순위검색.py
@@ -1,25 +1,40 @@
+from copy import deepcopy
 def parsed_info(infos):
     answer = {}
     for info in infos:
         info = info.split()
         score = int(info.pop())
-        query = " ".join(info)
-        if query not in answer:
-            answer[query] = [score]
-        else:
-            answer[query].append(score)
+        
+        for i in range(len(info)):
+            temp = deepcopy(info)
+            temp[i] = "-"
+            query = " ".join(temp)
+            if query not in answer:
+                answer[query] = [score]
+            else:
+                answer[query].append(score)
     return answer
 
-def parsed_query(query):
+def parsed_query(queries):
     answer = []
-    for q in query:
-        q = q.replace(" and "," ")
-        q = q.split()
-        score = int(q.pop())
-        answer.append([" ".join(q), score])
+    for query in queries:
+        query = query.replace(" and "," ")
+        query = query.split()
+        score = int(query.pop())
+        answer.append([" ".join(query), score])
     return answer
 
 def solution(info, query):
-    query = parsed_query(query)
+    answer = []
     info = parsed_info(info)
-    return info
+    
+    for query, std_score in parsed_query(query):
+        cnt = 0
+        if query not in info:
+            answer.append(cnt)
+            continue
+        for data_score in info[query]:
+            if data_score >= std_score:
+                cnt+=1
+        answer.append(cnt)
+    return answer
