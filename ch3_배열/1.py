279694c457eaaca0fa9825878d03720fd0650dc2
mutjin08 <mutjin08@gmail.com>
2024-01-17 13:15:19 +0900

    24-01-17

--- ch3_배열/1.py
+++
@@ -1,7 +0,0 @@
-def solution(rows, columns, queries):
-    board = [[i*columns+j+1 for j in range(columns)] for i in range(rows)]
-    for x1, y1, x2, y2 in queries:
-        temp = board[x1][y2]
-        for y in range(y1, y2):
-            board[x1][y] = 
-    return board
