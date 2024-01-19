56564e94c18266dda73e9bb5fb57544e55e36c6e
mutjin08 <mutjin08@gmail.com>
2024-01-19 10:10:44 +0900

    24-01-19

--- ch9_해시/72084_오픈 채팅방.py
+++ ch9_해시/72084_오픈 채팅방.py
@@ -3,24 +3,23 @@ def id_to_name(logs, users):
         logs[i][0] = users[logs[i][0]]
         logs[i] = "".join(logs[i])
     return logs
-    
 
 def solution(record):
     answer = []
     users = {}
     for r in record:
-        temp = r.split()
-        if len(temp)==3:
-            action, user_id, user_name = temp
-        elif len(r)==2:
-            action, user_id = temp
-        
-        if action == "Enter":
+        r = r.split()
+        if len(r) == 3:
+            user_action, user_id, user_name = r
+        elif len(r) == 2:
+            user_action, user_id = r
+            
+        if user_action=="Enter":
+            answer.append([user_id, "님이 들어왔습니다."])
             users[user_id] = user_name
-            answer.append([user_id,"님이 나갔습니다."])
-        elif action == "Leave":
-            answer.append([user_id,"님이 들어왔습니다."])
-        elif action == "Change":
+        elif user_action=="Change":
             users[user_id] = user_name
-    
+        elif user_action == "Leave":
+            answer.append([user_id, "님이 나갔습니다."])
+            
     return id_to_name(answer, users)
