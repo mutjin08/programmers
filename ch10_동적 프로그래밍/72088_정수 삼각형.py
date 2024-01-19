57a79f4ce17ac61446028335ce544fefda92366e
mutjin08 <mutjin08@gmail.com>
2024-01-19 15:16:10 +0900

    24-01-19

--- ch10_동적 프로그래밍/72088_정수 삼각형.py
+++ ch10_동적 프로그래밍/72088_정수 삼각형.py
@@ -1,9 +1,9 @@
 def solution(triangle):
-    answer = 0
-    dp = [0]*len(triangle)
-    dp[0] = triangle[0][0]
+    dp = [[0 for _ in range(i+1)] for i in range(len(triangle))]
+    dp[0][0] = triangle[0][0]
     for h in range(1, len(triangle)):
-        dp[h] = dp[h-1]+triangle[h][0]
-        for i in range(1, h+1):
-            dp[h] = max(dp[h],dp[h-1]+triangle[h][i-1], dp[h-1]+triangle[h][i])
-    return dp
+        dp[h][0] = dp[h-1][0] + triangle[h][0]
+        for i in range(1, h):
+            dp[h][i] = max(dp[h][i], dp[h-1][i-1]+triangle[h][i], dp[h-1][i]+triangle[h][i])
+        dp[h][h] = dp[h-1][-1] + triangle[h][h]
+    return max(dp[-1])
