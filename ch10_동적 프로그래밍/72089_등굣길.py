9312f30400338f86afbec9a62dc8b069cfec8a04
mutjin08 <mutjin08@gmail.com>
2024-01-19 16:24:40 +0900

    24-01-19

--- ch10_동적 프로그래밍/72089_등굣길.py
+++ ch10_동적 프로그래밍/72089_등굣길.py
@@ -1,21 +1,20 @@
-# error
 from collections import deque
 
 def bfs(m, n, puddles):
     dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-    for i in range(1, n+1):
-        dp[i][1] = 1
-    for j in range(1, m+1):
-        dp[1][j] = 1
+    dp[1][1] = 1
     
-    for r in range(2, n+1):
-        for c in range(2, m+1):
-            if [c, r-1] not in puddles:
-                dp[r][c]+=dp[r-1][c]
-            if [c-1, r] not in puddles:
-                dp[r][c]+=dp[r][c-1]
+    for r in range(1, n+1):
+        for c in range(1, m+1):
+            if r==1 and c==1:
+                continue
+            if [c, r] in puddles:
+                dp[r][c] = 0
+                continue
+            
+            dp[r][c] = (dp[r-1][c] + dp[r][c-1]) % 1000000007
 
-    return dp[n][m]%1000000007
+    return dp[n][m]
 
 def solution(m, n, puddles):
     return bfs(m, n, puddles)
