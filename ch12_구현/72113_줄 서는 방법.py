92ffdb74b030bdc9eccb016e8cc76770f75bbc2c
mutjin08 <mutjin08@gmail.com>
2024-01-22 15:39:26 +0900

    24-01-22

--- ch12_구현/72113_줄 서는 방법.py
+++ ch12_구현/72113_줄 서는 방법.py
@@ -1,8 +1,25 @@
-# 효율성 실패
+# 효율성 실패 -> 성공 but 다시풀기
 from itertools import permutations
+
+def factorial(n):
+    if n==1:
+        return 1
+    dp = [1]*(n+1)
+    for i in range(2, n+1):
+        dp[i] = dp[i-1]*i
+    return dp[n]
+
 def solution(n, k):
-    cnt = 1
-    for case in permutations([i+1 for i in range(n)]):
-        if cnt==k:
-            return list(case)
-        cnt += 1
+    answer = []
+    
+    
+    people = [i+1 for i in range(n)]
+    while n>0:
+        fact = factorial(n-1)
+        answer.append(people.pop((k-1)//fact))
+        k %= fact
+        n -= 1
+        
+        
+    return answer
+        
