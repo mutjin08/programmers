978595ddc679f24182245d9fab6b7bdb782659cc
mutjin08 <mutjin08@gmail.com>
2024-01-23 09:59:13 +0900

    24-01-23

--- ch12_구현/72115_여행 경로.py
+++ ch12_구현/72115_여행 경로.py
@@ -1,25 +1,24 @@
 from collections import deque
-
 def solution(tickets):
     tickets.sort(key = lambda x : (x[0], x[1]))
-    q = deque(["ICN", tickets])
+    q = deque([[["ICN"], tickets]])
     
     while q:
         now_paths, now_tickets = q.popleft()
         
-        if not now_tickets:
+        if len(now_tickets) == 0:
             return now_paths
         
-        idx = -1
+        valid_idx = -1
         for i in range(len(now_tickets)):
             if now_tickets[i][0] == now_paths[-1]:
-                idx = i
+                valid_idx = i
                 break
         
-        if idx == -1:
+        if valid_idx == -1:
             continue
-            
-        while idx<=len(now_tickets) and now_tickets[idx][0] == now_paths[-1]:
-            now_paths.append(now_tickets.pop(idx)[1])
-            q.append([now_paths, now_tickets])
-    return tickets
+        
+        while valid_idx < len(now_tickets) and now_tickets[valid_idx][0] == now_paths[-1]:
+            # 주의 1 : now_paths, now_tickets 재사용되어야 해서 건들노
+            q.append([now_paths+[now_tickets[valid_idx][1]], now_tickets[:valid_idx]+now_tickets[valid_idx+1:]])
+            valid_idx += 1 #주의 2 : 계속 키워줘야됨
