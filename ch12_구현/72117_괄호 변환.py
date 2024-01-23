9ba3b9be182979f9907e55f5dc15f0fa330f888a
mutjin08 <mutjin08@gmail.com>
2024-01-23 15:54:57 +0900

    24-01-23

--- ch12_구현/72117_괄호 변환.py
+++ ch12_구현/72117_괄호 변환.py
@@ -1,52 +1,43 @@
-def make_balanced(target):
-    l, r = 0, 0
-    for i in range(len(target)):
-        if target[i]=="(":
-            l+=1
-        elif target[i]==")":
-            r+=1
-        if l==r:
-            return target[:i+1], target[i+1:]
-    
-    return target, ""
-
-def is_balanced(target):
-    # 예외처리
-    if len(target)%2!=0:
-        return False
-    
-    if target.count('(')==target.count(')'):
-        return True
-    return False
-
-def is_correct(target):
-    # 예외처리
-    if target[0]==")" or target[-1]=="(":
-        return False
-    
-    if not is_balanced(target):
-        return False
-
+#주의 : 문자를 뒤집는거야 순서 거꾸로가 아니라
+def is_correct(w):
     cnt = 0
-    for c in target:
+    for c in w:
         if c=="(":
             cnt += 1
         elif c==")":
-            if cnt<1:
+            if cnt==0:
                 return False
-            elif cnt>=1:
+            else:
                 cnt -= 1
-    if cnt>0:
-        return False
-    return True
+    if cnt==0:
+        return True
+    return False
+
+def make_balanced(w):
+    l, r = 0, 0
+    for i in range(len(w)):
+        if w[i]=="(":
+            l+=1
+        elif w[i]==")":
+            r+=1
         
+        if l==r:
+            return w[:i+1], w[i+1:]
 
 def solution(w):
     if len(w)==0:
         return ""
+    
     u, v = make_balanced(w)
+    
     if is_correct(u):
         return u+solution(v)
     
-    u = "".join(reversed(u[1:len(u)-1]))
-    return "("+solution(v)+")"+u
+    temp = ""
+    for c in u[1:len(u)-1]:
+        if c=="(":
+            temp+=")"
+        elif c==")":
+            temp+="("
+    
+    return "("+solution(v)+")"+temp
