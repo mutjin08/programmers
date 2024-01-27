020de9dd746fbfa9440e8211418be2341f057f07
mutjin08 <mutjin08@gmail.com>
2024-01-27 20:58:06 +0900

    24-01-27

--- 프로그래머스 코딩 테스트 문제 풀이 전략 파이썬편/ch3_배열/72048_행렬 테두리 회전하기.py
+++ 프로그래머스 코딩 테스트 문제 풀이 전략 파이썬편/ch3_배열/72048_행렬 테두리 회전하기.py
@@ -1,18 +1,32 @@
+# 단순 회전 구현
 def spin_board(query, board):
     x1, y1, x2, y2 = [q-1 for q in query]
     
-    temp = board[x1][y1]
+    temp = target = board[x1][y1]
     for i in range(x1, x2):
         board[i][y1] = board[i+1][y1]
+        target = min(target, board[i][y1])
     for j in range(y1, y2):
         board[x2][j] = board[x2][j+1]
+        target = min(target, board[x2][j])
     for i in range(x2, x1, -1):
         board[i][y2] = board[i-1][y2]
+        target = min(target, board[i][y2])
     for j in range(y2, y1, -1):
         board[x1][j] = board[x1][j-1]
+        target = min(target, board[x1][j])
     board[x1][y1+1] = temp
     
-    return board
+    return target
+
+    
+
+def solution(rows, columns, queries):
+    answer = []
+    board = [[i*rows+j+1 for j in range(columns)] for i in range(rows)]
+    for query in queries:
+        answer.append(spin_board(query, board))
+    return answer
     
 
 def solution(rows, columns, queries):
