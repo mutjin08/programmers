e9c0c77427138798d4783457d9dba5a5121ec127
mutjin08 <mutjin08@gmail.com>
2024-01-27 17:19:28 +0900

    24-01-27

--- 프로그래머스 코딩 테스트 문제 풀이 전략 파이썬편/ch3_배열/72046_교점에 별 만들기.py
+++ 프로그래머스 코딩 테스트 문제 풀이 전략 파이썬편/ch3_배열/72046_교점에 별 만들기.py
@@ -1,131 +1,42 @@
+# 왜 board를 뒤집어서 내야 하는거지?
 from itertools import combinations
 
-def draw(spots):
-    mins, maxs = [], []
-    for target in zip(*spots):
-        mins.append(min(target))
-        maxs.append(max(target))
+def draw_star(spots):
+    minX, minY = [min(i) for i in zip(*spots)]
+    maxX, maxY = [max(i) for i in zip(*spots)]
     
-    x_size, y_size = maxs[0]-mins[0]+1, maxs[1]-mins[1]+1
-    board = [["." for _ in range(x_size)] for _ in range(y_size)]
+    sizeX = maxX - minX + 1
+    sizeY = maxY - minY + 1
+
+    board = [["." for _ in range(sizeX)] for _ in range(sizeY)]
+    for x, y in spots:
+        board[y-minY][x-minX] = "*"
+    
+    board = ["".join(i) for i in board][::-1]
     return board
+        
+
+def find_cross(l1, l2):
+    a, b, e = l1
+    c, d, f = l2
+    
+    if a*d-b*c==0:
+        return [False, -1, -1]
+    
+    x = (b*f-e*d)/(a*d-b*c)
+    y = (e*c-a*f)/(a*d-b*c)
+    
+    if x%1==0 and y%1==0:
+        return [True, int(x), int(y)]
+    
+    return [False, -1, -1]
 
 def solution(line):
     spots = []
-    for line1, line2 in combinations(line, 2):
-        a, b, e = line1
-        c, d, f = line2
-        if a*d-b*c==0:
-            continue
-        x = (b*f-e*d)/(a*d-b*c)
-        y = (e*c-a*f)/(a*d-b*c)
-        if x%1!=0 or y%1!=0:
-            continue
-        spots.append((int(x), int(y)))
+    for l1, l2 in combinations(line, 2):
+        result = find_cross(l1, l2)
+        if result[0]:
+            spots.append((result[1], result[2]))
     spots = list(set(spots))
-    
-    return draw(spots)from itertools import combinations
-
-def draw(spots):
-    mins, maxs = [], []
-    for target in zip(*spots):
-        mins.append(min(target))
-        maxs.append(max(target))
-    
-    x_size, y_size = maxs[0]-mins[0]+1, maxs[1]-mins[1]+1
-    board = [["." for _ in range(x_size)] for _ in range(y_size)]
-    return board
-
-def solution(line):
-    spots = []
-    for line1, line2 in combinations(line, 2):
-        a, b, e = line1
-        c, d, f = line2
-        if a*d-b*c==0:
-            continue
-        x = (b*f-e*d)/(a*d-b*c)
-        y = (e*c-a*f)/(a*d-b*c)
-        if x%1!=0 or y%1!=0:
-            continue
-        spots.append((int(x), int(y)))
-    spots = list(set(spots))
-    
-    return draw(spots)from itertools import combinations
-
-def draw(spots):
-    mins, maxs = [], []
-    for target in zip(*spots):
-        mins.append(min(target))
-        maxs.append(max(target))
-    
-    x_size, y_size = maxs[0]-mins[0]+1, maxs[1]-mins[1]+1
-    board = [["." for _ in range(x_size)] for _ in range(y_size)]
-    return board
-
-def solution(line):
-    spots = []
-    for line1, line2 in combinations(line, 2):
-        a, b, e = line1
-        c, d, f = line2
-        if a*d-b*c==0:
-            continue
-        x = (b*f-e*d)/(a*d-b*c)
-        y = (e*c-a*f)/(a*d-b*c)
-        if x%1!=0 or y%1!=0:
-            continue
-        spots.append((int(x), int(y)))
-    spots = list(set(spots))
-    
-    return draw(spots)from itertools import combinations
-
-def draw(spots):
-    mins, maxs = [], []
-    for target in zip(*spots):
-        mins.append(min(target))
-        maxs.append(max(target))
-    
-    x_size, y_size = maxs[0]-mins[0]+1, maxs[1]-mins[1]+1
-    board = [["." for _ in range(x_size)] for _ in range(y_size)]
-    return board
-
-def solution(line):
-    spots = []
-    for line1, line2 in combinations(line, 2):
-        a, b, e = line1
-        c, d, f = line2
-        if a*d-b*c==0:
-            continue
-        x = (b*f-e*d)/(a*d-b*c)
-        y = (e*c-a*f)/(a*d-b*c)
-        if x%1!=0 or y%1!=0:
-            continue
-        spots.append((int(x), int(y)))
-    spots = list(set(spots))
-    
-    return draw(spots)from itertools import combinations
-
-def draw(spots):
-    mins, maxs = [], []
-    for target in zip(*spots):
-        mins.append(min(target))
-        maxs.append(max(target))
-    
-    x_size, y_size = maxs[0]-mins[0]+1, maxs[1]-mins[1]+1
-    board = [["." for _ in range(x_size)] for _ in range(y_size)]
-    return board
-
-def solution(line):
-    spots = []
-    for line1, line2 in combinations(line, 2):
-        a, b, e = line1
-        c, d, f = line2
-        if a*d-b*c==0:
-            continue
-        x = (b*f-e*d)/(a*d-b*c)
-        y = (e*c-a*f)/(a*d-b*c)
-        if x%1!=0 or y%1!=0:
-            continue
-        spots.append((int(x), int(y)))
-    spots = list(set(spots))
-    
-    return draw(spots)
+    return draw_star(spots)
+        
---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략 파이썬편/ch3_배열/72048_행렬 테두리 회전하기.py
@@ -0,0 +1,23 @@
+def spin_board(query, board):
+    x1, y1, x2, y2 = [q-1 for q in query]
+    
+    temp = board[x1][y1]
+    for i in range(x1, x2):
+        board[i][y1] = board[i+1][y1]
+    for j in range(y1, y2):
+        board[x2][j] = board[x2][j+1]
+    for i in range(x2, x1, -1):
+        board[i][y2] = board[i-1][y2]
+    for j in range(y2, y1, -1):
+        board[x1][j] = board[x1][j-1]
+    board[x1][y1+1] = temp
+    
+    return board
+    
+
+def solution(rows, columns, queries):
+    board = [[i*rows+j+1 for j in range(columns)] for i in range(rows)]
+    for query in queries:
+        x = spin_board(query, board)
+        print(x)
+    return board
