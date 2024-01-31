42ebf5cc8089dd7b79c7036a4dfd230d26b52ecd
mutjin08 <mutjin08@gmail.com>
2024-01-31 10:21:55 +0900

    24-01-31

--- kakao/150368_이모티콘 할인행사.py
+++ kakao/150368_이모티콘 할인행사.py
@@ -1,7 +1,5 @@
+#product vs. combinations_with_replacement
 
-"""
-카카오톡 사용자 n명의 구매 기준을 담은 2차원 정수 배열 users, 이모티콘 m개의 정가를 담은 1차원 정수 배열 emoticons가 주어집니다. 이때, 행사 목적을 최대한으로 달성했을 때의 이모티콘 플러스 서비스 가입 수와 이모티콘 매출액을 1차원 정수 배열에 담아 return 하도록 solution 함수를 완성해주세요.
-"""
 from itertools import product
 
 def case_result(users, emoticons, discounts):
