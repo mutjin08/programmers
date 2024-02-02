24a39b16242403137a65782f3b7deb640044a70a
mutjin08 <mutjin08@gmail.com>
2024-02-02 17:33:49 +0900

    24-02-02

--- 코딩테스트 고득점 Kit/깊이,너비 우선 탐색(DFS,BFS)/43163_단어 변환.py
+++ 코딩테스트 고득점 Kit/깊이,너비 우선 탐색(DFS,BFS)/43163_단어 변환.py
@@ -1,26 +1,25 @@
 from collections import deque
 
-def count_diff(word1, word2):
-    cnt = 0
-    for a, b in zip(word1, word2):
-        if a!=b:
-            cnt+=1
-    if cnt==1:
-        return True
-    return False
-
 def solution(begin, target, words):
+    if target not in words:
+        return 0
     
-    q = deque([[begin, 0]])
-    visited = {}
-    visited[begin] = 1
-    
-    while q:
-        x, cnt = q.popleft()
-        for w in words:
-            if count_diff(x, w) and w not in visited:
-                if w==target:
-                    return cnt+1
-                q.append([w, cnt+1])
-                visited[w] = 1
+    queue = deque()
+    queue.append((begin, 0))
+
+    while queue:
+        x, cnt = queue.popleft()
+
+        if x == target:
+            return cnt
+        
+        for i in range(len(words)):
+            diff = 0
+            word = words[i]
+            for j in range(len(x)):
+                if x[j] != word[j]:
+                    diff += 1
+            if diff == 1:
+                queue.append((word, cnt + 1))
+
     return 0
