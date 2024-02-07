c7f1d9f8a0748d2f46257550763338e420a19929
mutjin08 <mutjin08@gmail.com>
2024-02-07 10:52:27 +0900

    24-02-07

--- 코딩테스트 고득점 Kit/깊이,너비 우선 탐색(DFS,BFS)/43163_단어 변환.py
+++ 코딩테스트 고득점 Kit/깊이,너비 우선 탐색(DFS,BFS)/43163_단어 변환.py
@@ -1,25 +1,27 @@
 from collections import deque
 
+def one_diff(a, b):
+    answer = 0
+    for i in range(len(a)):
+        if a[i]!=b[i]:
+            answer += 1
+    if answer==1:
+        return True
+    return False
+
 def solution(begin, target, words):
     if target not in words:
         return 0
     
-    queue = deque()
-    queue.append((begin, 0))
-
-    while queue:
-        x, cnt = queue.popleft()
-
+    q = deque([(begin, 0)])
+    while q:
+        print(q)
+        x, cnt = q.popleft()
         if x == target:
             return cnt
         
-        for i in range(len(words)):
-            diff = 0
-            word = words[i]
-            for j in range(len(x)):
-                if x[j] != word[j]:
-                    diff += 1
-            if diff == 1:
-                queue.append((word, cnt + 1))
-
+        for word in words:
+            if one_diff(x, word):
+                q.append((word, cnt+1))
     return 0
+    
